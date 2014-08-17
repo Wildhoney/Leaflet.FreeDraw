72ed41164071bc59d31516946f6a6480c00a4667
Wildhoney <adam.timberlake@gmail.com>
2014-08-17 15:31:17 +0100

    Added initial module

---
+++ modules/FreeDraw.js
@@ -0,0 +1,13 @@
+(function($window) {
+
+    /**
+     * @module FreeDraw
+     * @author Adam Timberlake
+     * @link https://github.com/Wildhoney/Leaflet.FreeDraw
+     * @constructor
+     */
+    $window.FreeDraw = function FreeDraw() {
+
+    };
+
+})(window);
