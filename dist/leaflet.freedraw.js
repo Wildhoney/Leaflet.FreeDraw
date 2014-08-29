d92ab6b5e6fcf19fc8d9a31d33710aec12a0f164
Wildhoney <adam.timberlake@gmail.com>
2014-08-30 00:32:27 +0100

    Added utilities object

--- dist/leaflet.freedraw.js
+++ dist/leaflet.freedraw.js
@@ -1512,3 +1512,47 @@
     };
 
 })(window, window.L, window.d3, window.ClipperLib);
+
+(function() {
+
+    "use strict";
+
+    /**
+     * @module FreeDraw
+     * @submodule Utilities
+     * @author Adam Timberlake
+     * @link https://github.com/Wildhoney/Leaflet.FreeDraw
+     */
+    L.FreeDraw.Utilities = {
+
+        /**
+         * Responsible for converting the multiple polygon points into a MySQL object for
+         * geo-spatial queries.
+         *
+         * @method getMySQLMultiPolygon
+         * @param latLngGroups {Array}
+         * @return {String}
+         */
+        getMySQLMultiPolygon: function getMySQLMultiPolygon(latLngGroups) {
+
+            var groups = [];
+
+            latLngGroups.forEach(function forEach(latLngs) {
+
+                var group = [];
+
+                latLngs.forEach(function forEach(latLng) {
+                    group.push(latLng.lat + ' ' + latLng.lng);
+                });
+
+                groups.push('((' + group.join(',') + '))');
+
+            });
+
+            return 'MULTIPOLYGON(' + groups.join(',') + ')';
+
+        }
+
+    };
+
+})();
