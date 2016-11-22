898cfdaab45d43254c15d7421f413782bd197cc9
Wildhoney <adam.timberlake@gmail.com>
2016-11-22 19:53:47 +0000

    Removed initial code

--- example/js/default.js
+++ example/js/default.js
@@ -1,18 +1,5 @@
 import L from 'leaflet';
 import FreeDraw, { VIEW, CREATE, EDIT, DELETE, APPEND, ALL } from '../../src/FreeDraw';
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//
-//     const map = new L.Map(document.querySelector('section.map'), { doubleClickZoom: false }).setView([51.505, -0.09], 14);
-//     L.tileLayer('https://tiles.lyrk.org/lr/{z}/{x}/{y}?apikey=f2ae86661a4e487bbced29a755799884').addTo(map);
-//
-//     const freeDraw = new FreeDraw({
-//         mode: CREATE | EDIT | DELETE | APPEND
-//     });
-//
-//     map.addLayer(freeDraw);
-//
-// });
 
 import { module } from 'angular';
 
