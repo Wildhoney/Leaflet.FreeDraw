7d625996ebf506df2ca05bc0d5e365c3700ed50d
Wildhoney <adam.timberlake@gmail.com>
2016-11-24 21:06:50 +0000

    Added functional Nightmare tests

---
+++ tests/functional/FreeDraw.test.js
@@ -0,0 +1,65 @@
+import Nightmare from 'nightmare';
+import { resolve } from 'path';
+import { expect } from 'chai';
+import { createFirstPolygon, createSecondPolygon, createMergedPolygon, removeFirstPolygon } from './helpers/Polygons';
+
+// Instantiate Nightmare.
+const nightmare = Nightmare({ show: false });
+
+// Fetch the absolute path to the index page.
+const url = `file://${resolve('example/index.html')}`;
+
+/**
+ * @method error
+ * @param {Function} done
+ * @return {Function}
+ */
+function error(done) {
+
+    return err => {
+        console.log(err);
+        done();
+    };
+
+}
+
+describe('FreeDraw', () => {
+
+    it('It should be able to create separate polygons;', done => {
+
+        nightmare
+            .goto(url)
+            .wait('svg.free-draw')
+            .evaluate(createMergedPolygon)
+            .then(polygons => expect(polygons.length).to.equal(1))
+            .then(() => done())
+            .catch(error(done));
+
+    });
+
+    it('It should be able to create merged polygons;', done => {
+
+        nightmare
+            .goto(url)
+            .wait('svg.free-draw')
+            .evaluate(createFirstPolygon)
+            .evaluate(createSecondPolygon)
+            .then(polygons => expect(polygons.length).to.equal(2))
+            .then(() => done())
+            .catch(error(done));
+
+    });
+
+    it('It should be able to delete polygon;', done => {
+
+        nightmare
+            .goto(url)
+            .wait('svg.free-draw')
+            .evaluate(removeFirstPolygon)
+            .then(polygons => expect(polygons.length).to.equal(1))
+            .then(() => done())
+            .catch(error(done));
+
+    });
+
+});
---
+++ tests/functional/helpers/Polygons.js
@@ -0,0 +1,60 @@
+export const createMergedPolygon = () => {
+
+    const map = document.querySelector('section.map');
+
+    map.dispatchEvent(new MouseEvent('mousedown'));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 100, clientY: 100 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 100, clientY: 200 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 200, clientY: 200 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 200, clientY: 100 }));
+    map.dispatchEvent(new MouseEvent('mouseup'));
+
+    map.dispatchEvent(new MouseEvent('mousedown'));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 150, clientY: 150 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 150, clientY: 250 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 250, clientY: 250 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 250, clientY: 150 }));
+    map.dispatchEvent(new MouseEvent('mouseup'));
+
+    return Array.from(document.querySelectorAll('path.leaflet-polygon'));
+
+};
+
+export const createFirstPolygon = () => {
+
+    const map = document.querySelector('section.map');
+
+    map.dispatchEvent(new MouseEvent('mousedown'));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 50, clientY: 50 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 50, clientY: 100 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 100, clientY: 100 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 100, clientY: 50 }));
+    map.dispatchEvent(new MouseEvent('mouseup'));
+
+    return Array.from(document.querySelectorAll('path.leaflet-polygon'));
+
+};
+
+export const createSecondPolygon = () => {
+
+    const map = document.querySelector('section.map');
+
+    map.dispatchEvent(new MouseEvent('mousedown'));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 150, clientY: 150 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 150, clientY: 250 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 250, clientY: 250 }));
+    map.dispatchEvent(new MouseEvent('mousemove', { clientX: 250, clientY: 150 }));
+    map.dispatchEvent(new MouseEvent('mouseup'));
+
+    return Array.from(document.querySelectorAll('path.leaflet-polygon'));
+
+};
+
+export const removeFirstPolygon = () => {
+
+    const polygon = Array.from(document.querySelectorAll('path.leaflet-polygon'))[1];
+    polygon.dispatchEvent(new MouseEvent('click'));
+
+    return Array.from(document.querySelectorAll('path.leaflet-polygon'));
+
+};
