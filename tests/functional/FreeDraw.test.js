eda3edfa647b41343c0b232b8f7a4dc81d0d1795
Wildhoney <adam.timberlake@gmail.com>
2016-11-26 00:05:34 +0000

    Reduced timeout to 30s

--- tests/functional/FreeDraw.test.js
+++ tests/functional/FreeDraw.test.js
@@ -7,7 +7,7 @@ import { createFirstPolygon, createSecondPolygon, createMergedPolygon, removeFir
  * @constant timeout
  * @type {Number}
  */
-const timeout = 60000;
+const timeout = 30000;
 
 // Instantiate Nightmare.
 const nightmare = Nightmare({ show: false, frame: false });
