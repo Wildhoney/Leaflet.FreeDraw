16bef9b817f4996b690672c714d6d155ce661450
Wildhoney <adam.timberlake@gmail.com>
2016-12-03 00:37:27 +0000

    Added gzip compression to Express server

--- example/server/default.js
+++ example/server/default.js
@@ -1,8 +1,11 @@
 import http from 'http';
 import express from 'express';
+import compression from 'compression';
 import opener from 'opener';
 
 const app = express();
+app.use(compression());
+
 const server = http.createServer(app);
 const isHeroku = 'HEROKU_APP_NAME' in process.env;
 
