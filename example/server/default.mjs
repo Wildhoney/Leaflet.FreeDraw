0c159ce874b77938732d25c58c7b534fc8a18d5b
Wildhoney <adam.timberlake@gmail.com>
2019-03-20 09:27:43 +0000

    Removed `opener` dependency

--- example/server/default.mjs
+++ example/server/default.mjs
@@ -2,16 +2,12 @@ import path from 'path';
 import http from 'http';
 import express from 'express';
 import compression from 'compression';
-import opener from 'opener';
 
 const app = express();
-app.use(compression());
-
 const server = http.createServer(app);
-const isHeroku = 'HEROKU_APP_NAME' in process.env;
 
+app.use(compression());
 app.use(express.static(path.resolve('./example')));
 
 const port = process.env.PORT || 5000;
 server.listen(port);
-!isHeroku && opener(`http://localhost:${port}`);
