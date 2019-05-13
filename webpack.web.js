c1dc442dcb03450a1752e9973eaf521aa703187b
Ankeet Maini <ankeet.maini@gmail.com>
2019-05-13 15:37:27 +0530

    Modifies build script for easier debugging.

--- webpack.web.js
+++ webpack.web.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: ['core-js', '@babel/polyfill', path.resolve('./src/FreeDraw.js')],
     mode: process.env.NODE_ENV || 'production',
+    devtool: 'eval-source-map',
     output: {
         filename: 'leaflet-freedraw.web.js',
         path: path.resolve('./dist'),
