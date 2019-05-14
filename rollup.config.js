5b94c9320bfe2414d0d43db225c7a6725f252435
Ankeet Maini <ankeet.maini@gmail.com>
2019-05-14 16:20:05 +0530

    Uses rollup for bundling

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
+
+module.exports = {
+  input: 'src/FreeDraw.js',
+  output: [
+    {
+      file: 'dist/leaflet-freedraw.cjs.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      external: ['ramda', 'leaflet', 'clipper-lib'],
+    },
+    {
+      file: 'dist/leaflet-freedraw.esm.js',
+      format: 'esm',
+      sourcemap: true,
+      exports: 'named',
+      external: ['ramda', 'leaflet'],
+    },
+    {
+      file: 'dist/leaflet-freedraw.web.js',
+      format: 'iife',
+      sourcemap: true,
+      exports: 'named',
+      name: 'LeafletFreeDraw',
+      external: ['ramda', 'leaflet'],
+    }
+  ],
+  plugins: [ 
+    resolve(),
+    commonjs({
+      namedExports: {
+        'node_modules/leaflet/dist/leaflet-src.js': [ 'DomUtil', 'Point', 'DivIcon', 'Marker', 'DomEvent', 'Polygon', 'LineUtil', 'FeatureGroup' ],
+        'node_modules/clipper-lib/clipper.js': ['Clipper', 'PolyFillType'],
+        'node_modules/ramda/dist/ramda.js': ['flatten', 'compose', 'head', 'complement', 'identical']
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    terser()
+  ]
+};
