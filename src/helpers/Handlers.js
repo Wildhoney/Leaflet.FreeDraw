aaa6d969e60f36f00e4c9d5c458db6a627aa5627
Sagarpreet <53554917+Sagarpreet@users.noreply.github.com>
2019-09-25 12:53:31 +0530

    Feature ps (#7)
    * modes working fine
    
    * working
    
    * working great
    
    * Delete markers done
    
    * made another MODE for deleting 1 point only
    
    * deleted package-lock.json
    
    * added package-lock.json
    
    * undo_redo working with Delete markers
    
    * Undo Redo works with Delete 1 marker
    
    * works great
    
    * works great
    
    * pubsub implememnted
    
    * implemented
    
    * minor change

---
+++ src/helpers/Handlers.js
@@ -0,0 +1,137 @@
+import { rawLatLngKey, polygonID, polygons } from '../FreeDraw';
+import { removeFor, createFor } from './Polygon';
+import Stack from './Stack';
+import {undoMainStack, undoStackObject, redoMainStack, redoStackObject, mergedPolygonsMap} from './UndoRedo';
+
+
+// The current Polygon is merged Polygon .
+// Add the merged polygon in Undo Stack which is mapped to [intersectingPolygons - current Polygon]
+export const mergedPolygonCreatedHandler = (data) => {
+    undoMainStack.push(data.count);
+    undoStackObject[data.count] = Stack();
+    undoStackObject[data.count].push(data.addedPolygons[0]);
+    data.options.mergedFromPolygons && (mergedPolygonsMap[data.count] = data.options.mergedFromPolygons);
+    return;
+}
+
+// the current polygon came from Undo . (special Case)
+export const mergePolygonUndoHandler = (data) => {
+    if (!undoStackObject[data.pid]) {
+        undoStackObject[data.pid] = new Stack();
+    }
+    // Remove from undoStackObject the latest state of pid .
+    undoStackObject[data.pid].pop();
+    // Add the new Polygon which has now listeners attached to undoMainStack .
+    undoStackObject[data.pid].push(data.addedPolygons[0]);
+    return;
+}
+
+// Redo operation performed on a merged Polygon.
+// Add the merged polygon in Undo Stack which is mapped to [intersectingPolygons - current Polygon]
+export const mergePolygonRedoHandler = (data) => {
+    undoMainStack.push(data.pid);
+    undoStackObject[data.pid] = Stack();
+    undoStackObject[data.pid].push(data.addedPolygons[0]);
+    data.options.mergedFromPolygons && (mergedPolygonsMap[data.pid] = data.options.mergedFromPolygons);
+    return;   
+}
+
+// Polygon is not overlapping!
+export const newPolygonCreatedHandler = (data) => {
+    console.log("CASE DEFAULT 2");
+    // new Polygon is created -> Clear REDO Stack .
+    redoMainStack.clear();
+    redoStackObject.clear();
+
+    const limitReached = polygons.get(data.map).size === data.options.maximumPolygons;
+     // if new Polygon is created and it is intersecting -> do not add to Undo Stack .    ;
+    if (data.isIntersecting && !limitReached && !data.preventMutations && polygons.get(data.map).size > 1 && data.options.mergePolygons) {
+        return;
+    }
+
+    undoStackObject[data.count] = Stack();
+    undoStackObject[data.count].push(data.addedPolygons[0]);
+    undoMainStack.push(data.count);
+    return;
+}
+
+// Polygon is not overlapping!
+export const existingPolygonEditedHandler = (data) => {
+    console.log("CASE DEFAULT 1");
+    if (!undoStackObject[data.pid]) {
+        undoStackObject[data.pid] = new Stack();
+    }
+
+    // comes in edit mode and does not merges/ self-intersects AND add to main Stack .
+    if(!data.from) {
+        undoMainStack.push(data.pid);
+    }
+    
+    // comes in Undo Listener and does not merges/ self-intersects .
+    undoStackObject[data.pid].push(data.addedPolygons[0]);
+    return;
+}
+
+export const undoHandler = map => {
+
+    // DELETE POLYGON AND HANDLING STATES LOGIC :
+
+    // Pop from Undo_Main_Stack and push to Redo_Main_Stack .
+        const id = undoMainStack.length ? undoMainStack.pop() : 0;
+        id && redoMainStack.push(id);
+
+    // Pop the polygon currently visible on Screen via the "id" from the undo_stack_Object.
+        const undoPoppedEl = id && undoStackObject[id] ? undoStackObject[id].pop() : 0;
+        if (!redoStackObject[id]) {
+            redoStackObject[id] = new Stack();
+        }
+
+    // Add the polygon to redo_stack_Object and remove the popped polygon from the screen.
+        undoPoppedEl && redoStackObject[id].push(undoPoppedEl);
+        undoPoppedEl && removeFor(map, undoPoppedEl);
+
+    // CREATE POLYGON LOGIC :
+
+    // check if Undo_Main_Stack is not empty and corresponding undo_stack_Object is also not empty.
+        if (id && !undoStackObject[id].empty()) {
+
+        // The new Polygon to be created is now at the top of undo_stack_Object.
+            const revertedToPoly = undoStackObject[id].pop();
+            createFor(map, revertedToPoly[rawLatLngKey], revertedToPoly._options, true, id, 1);
+
+        } else if (mergedPolygonsMap[id] && mergedPolygonsMap[id].length !== 0) {
+      // Current Polygon is a merged polygon, so create polygons from which it is made.
+            mergedPolygonsMap[id].forEach(element => {
+                createFor(map, element[rawLatLngKey], element._options, true, element[polygonID], 3); // Should not Update undoStack State .
+            });
+        }
+    };
+
+    export const redoHandler = map => {
+    // Pop the id of polygon and the polygon itself from Redo_Main_Stack and redo_stack_Object respectively.
+        const id = redoMainStack.pop();
+        const redoPoppedEl = id ? redoStackObject[id].pop() : 0;
+
+    // Get the element that we have to remove which is currently on the Screen.
+        const currentEl = id && undoStackObject[id].length() ? undoStackObject[id].top() : 0;
+
+        if (currentEl) {
+      // remove the polygon currently on Screen.
+            removeFor(map, currentEl);
+
+      // Create the polygon which we had popped from redo_stack_Object.
+            redoPoppedEl && createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 0);
+
+        } else if (mergedPolygonsMap[id] && mergedPolygonsMap[id].length !== 0) {
+      // The polygon to be made is a merged Polygon, hence removing all the polygons which are overlapping (merged) and then creating the merged polygon.
+            mergedPolygonsMap[id].forEach(element => {
+                removeFor(map, undoStackObject[element[polygonID]].top());
+            });
+            createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 4);
+
+        } else {
+        // If Polygon has no State in UNDO STACK -> Simply draw REDO polygon
+            redoPoppedEl && createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 0);
+        }
+
+    };
--- src/helpers/UndoRedo.js
+++ src/helpers/UndoRedo.js
@@ -1,6 +1,6 @@
-import { rawLatLngKey, polygonID, polygons } from '../FreeDraw';
-import { removeFor, createFor } from './Polygon';
 import Stack from './Stack';
+import { pubSub } from './PubSub';
+import {mergedPolygonCreatedHandler, mergePolygonUndoHandler, mergePolygonRedoHandler, newPolygonCreatedHandler, existingPolygonEditedHandler, undoHandler, redoHandler} from './Handlers';
 
 // Undo MAIN Stack that stores the sequence in which Polygons are added/edited.
 export const undoMainStack = Stack();
@@ -17,6 +17,11 @@ export const redoStackObject = new Map();
 // Map which holds the Polygons to made when Undo operation is perfromed on Merged Polygon .
 export const mergedPolygonsMap = new Map();
 
+pubSub.subscribe("Polygon_made_is_overlapping_with_other_polygons", mergedPolygonCreatedHandler);
+pubSub.subscribe("Polygon_to_undo_is_merged_polygon", mergePolygonUndoHandler);
+pubSub.subscribe("Polygon_to_redo_is_merged_polygon", mergePolygonRedoHandler);
+pubSub.subscribe("Simple_new_Polygon_is_created", newPolygonCreatedHandler);
+pubSub.subscribe("Existing_Polygon_is_edited_and_it_is_non_overlapping", existingPolygonEditedHandler);
 
 /*
 from = 0 : When existing polygon is edited -> comes from Polyfill() in Merge.js
@@ -24,64 +29,31 @@ from = 1 : When Undo operation is performed -> comes from UndoRedoDS.js
 from = 2 : When new Polygon is created AND it is intersecting -> comes from Merge() in Merge.js
 from = 3 : When Undo operation is performed on a Merged polygon -> comes from UndoRedo.js
 */
-export function maintainStackStates(map, addedPolygons, options, preventMutations, isIntersecting, count, pid, from) {
-    const limitReached = polygons.get(map).size === options.maximumPolygons;
-
-    // if new Polygon is created and it is intersecting -> do not add to Undo Stack .    ;
-    if (isIntersecting && !limitReached && !preventMutations && polygons.get(map).size > 1 && options.mergePolygons) {
-        redoMainStack.clear();
-        redoStackObject.clear();
-        return;
-    }
-    // The current Polygon is merged Polygon .
-    // Add the merged polygon in Undo Stack which is mapped to [intersectingPolygons - current Polygon]
-    switch (from) {
+export function maintainStackStates(data) {
+    console.log(data);
+    switch (data.from) {
         case 2 : {
-            undoMainStack.push(count);
-            undoStackObject[count] = Stack();
-            undoStackObject[count].push(addedPolygons[0]);
-            options.mergedFromPolygons && (mergedPolygonsMap[count] = options.mergedFromPolygons);
+            console.log("CASE 2");
+            pubSub.publish("Polygon_made_is_overlapping_with_other_polygons", data);
             return;
         }
-        // the current polygon came from Undo . (special Case)
         case 3: {
-            // Remove from undoStackObject the latest state of pid .
-            undoStackObject[pid] && undoStackObject[pid].pop();
-            // Add the new Polygon which has now listeners attached to undoMainStack .
-            undoStackObject[pid].push(addedPolygons[0]);
+            console.log("CASE 3");
+            pubSub.publish("Polygon_to_undo_is_merged_polygon", data);
             return;
         }
-        // The current Polygon is merged Polygon .
-        // Add the merged polygon in Undo Stack which is mapped to [intersectingPolygons - current Polygon]
         case 4: {
-            undoMainStack.push(pid);
-            undoStackObject[pid] = Stack();
-            undoStackObject[pid].push(addedPolygons[0]);
-
-            options.mergedFromPolygons && (mergedPolygonsMap[pid] = options.mergedFromPolygons);
+            console.log("CASE 4");
+            pubSub.publish("Polygon_to_redo_is_merged_polygon", data);
             return;
         }
         default: {
-            // comes in edit mode and does not merges/ self-intersects AND add to main Stack .
-            if (pid && addedPolygons.length === 1 && from === 0) {
-                undoMainStack.push(pid);
-                if (!undoStackObject[pid]) {
-                    undoStackObject[pid] = new Stack();
-                }
-                undoStackObject[pid].push(addedPolygons[0]);
-            } else if (pid && addedPolygons.length === 1 && from === 1) {
-                // comes in Undo Listener and does not merges/ self-intersects .
-                undoStackObject[pid].push(addedPolygons[0]);
+            if(data.pid) {
+                pubSub.publish("Existing_Polygon_is_edited_and_it_is_non_overlapping", data);
             } else {
-                // new Polygon is created -> Clear REDO Stack .
-                redoMainStack.clear();
-                redoStackObject.clear();
-                addedPolygons.forEach(p => {
-                    undoStackObject[count] = Stack();
-                    undoStackObject[count].push(p);
-                    undoMainStack.push(count);
-                });
+                pubSub.publish("Simple_new_Polygon_is_created", data);
             }
+            return;
         }
     }
     // console.log("UNDO Stack : " + undoMainStack.show());
@@ -97,70 +69,6 @@ export function clearAllStacks() {
 }
 
 export default function UndoRedo() {
-
-    const undoHandler = map => {
-
-    // DELETE POLYGON AND HANDLING STATES LOGIC :
-
-    // Pop from Undo_Main_Stack and push to Redo_Main_Stack .
-        const id = undoMainStack.length ? undoMainStack.pop() : 0;
-        id && redoMainStack.push(id);
-
-    // Pop the polygon currently visible on Screen via the "id" from the undo_stack_Object.
-        const undoPoppedEl = id && undoStackObject[id] ? undoStackObject[id].pop() : 0;
-        if (!redoStackObject[id]) {
-            redoStackObject[id] = new Stack();
-        }
-
-    // Add the polygon to redo_stack_Object and remove the popped polygon from the screen.
-        undoPoppedEl && redoStackObject[id].push(undoPoppedEl);
-        undoPoppedEl && removeFor(map, undoPoppedEl);
-
-    // CREATE POLYGON LOGIC :
-
-    // check if Undo_Main_Stack is not empty and corresponding undo_stack_Object is also not empty.
-        if (id && !undoStackObject[id].empty()) {
-
-        // The new Polygon to be created is now at the top of undo_stack_Object.
-            const revertedToPoly = undoStackObject[id].pop();
-            createFor(map, revertedToPoly[rawLatLngKey], revertedToPoly._options, true, id, 1);
-
-        } else if (mergedPolygonsMap[id] && mergedPolygonsMap[id].length !== 0) {
-      // Current Polygon is a merged polygon, so create polygons from which it is made.
-            mergedPolygonsMap[id].forEach(element => {
-                createFor(map, element[rawLatLngKey], element._options, true, element[polygonID], 3); // Should not Update undoStack State .
-            });
-        }
-    };
-
-    const redoHandler = map => {
-    // Pop the id of polygon and the polygon itself from Redo_Main_Stack and redo_stack_Object respectively.
-        const id = redoMainStack.pop();
-        const redoPoppedEl = id ? redoStackObject[id].pop() : 0;
-
-    // Get the element that we have to remove which is currently on the Screen.
-        const currentEl = id && undoStackObject[id].length() ? undoStackObject[id].top() : 0;
-
-        if (currentEl) {
-      // remove the polygon currently on Screen.
-            removeFor(map, currentEl);
-
-      // Create the polygon which we had popped from redo_stack_Object.
-            redoPoppedEl && createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 0);
-
-        } else if (mergedPolygonsMap[id] && mergedPolygonsMap[id].length !== 0) {
-      // The polygon to be made is a merged Polygon, hence removing all the polygons which are overlapping (merged) and then creating the merged polygon.
-            mergedPolygonsMap[id].forEach(element => {
-                removeFor(map, undoStackObject[element[polygonID]].top());
-            });
-            createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 4);
-
-        } else {
-        // If Polygon has no State in UNDO STACK -> Simply draw REDO polygon
-            redoPoppedEl && createFor(map, redoPoppedEl[rawLatLngKey], redoPoppedEl._options, true, id, 0);
-        }
-
-    };
 
     return {
         attachListeners(map) {
