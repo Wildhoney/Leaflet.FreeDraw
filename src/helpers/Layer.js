67b4d638adf536707260bf37e3c9b73db54e9e58
stereobooster <stereobooster@gmail.com>
2019-10-07 12:25:56 +0200

    Fix #146 - polygons.get(map) can return undefined

--- src/helpers/Layer.js
+++ src/helpers/Layer.js
@@ -10,16 +10,18 @@ import { NONE, CREATE, EDIT, DELETE, APPEND } from './Flags';
  */
 export const updateFor = (map, eventType) => {
 
-    const latLngs = Array.from(polygons.get(map)).map(polygon => {
+    if (polygons.get(map)) {
+        const latLngs = Array.from(polygons.get(map)).map(polygon => {
 
-        // Ensure the polygon has been closed.
-        const latLngs = polygon.getLatLngs();
-        return [ ...latLngs[0], latLngs[0][0] ];
+            // Ensure the polygon has been closed.
+            const latLngs = polygon.getLatLngs();
+            return [ ...latLngs[0], latLngs[0][0] ];
 
-    });
+        });
 
-    // Fire the current set of lat lngs.
-    map[instanceKey].fire('markers', { latLngs, eventType });
+        // Fire the current set of lat lngs.
+        map[instanceKey].fire('markers', { latLngs, eventType });
+    }
 
 };
 
